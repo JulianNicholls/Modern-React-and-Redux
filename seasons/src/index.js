8f4c7e751159bd3c733a7b0f9c2bd51172efa0d0
Julian Nicholls <juliannicholls29@gmail.com>
2019-11-06 20:26:44 +0000

    Display user position in Seasons

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,8 +2,32 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class App extends React.Component {
+  state = { position: null };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ position });
+      },
+      error => {
+        console.error({ error });
+      }
+    );
+  }
+
   render() {
-    return <div>Seasons start</div>;
+    return (
+      <div>
+        Seasons start
+        {this.state.position && (
+          <div>
+            <div>Lat: {this.state.position.coords.latitude}</div>
+            <div>Long: {this.state.position.coords.longitude}</div>
+            <div>+- {this.state.position.coords.accuracy}m</div>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 render(<App />, document.getElementById('root'));
