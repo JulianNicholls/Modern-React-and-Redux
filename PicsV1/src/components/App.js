410f9f104cf40130c68f17dccee4775fc6c4408b
Julian Nicholls <juliannicholls29@gmail.com>
2019-11-07 21:33:40 +0000

    Create and use custom axios instance

--- PicsV1/src/components/App.js
+++ PicsV1/src/components/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import SearchBar from './SearchBar';
+import ImageDisplay from './ImageDisplay';
+
+import unsplash from '../api/unsplash';
 
 const App = () => {
-  const setSearchTerm = term => {
-    console.log({ term });
+  const [images, setImages] = useState([]);
+
+  const loadImages = async query => {
+    console.log({ query });
+
+    const response = await unsplash.get('/search/photos', {
+      params: { query, per_page: 15 },
+    });
+
+    console.log({ response });
+
+    setImages(response.data.results.map(({ urls }) => urls));
   };
 
   return (
     <div className="ui container" style={{ marginTop: '1rem' }}>
-      <SearchBar onSubmit={setSearchTerm} />
+      <SearchBar onSubmit={loadImages} />
+      Found: {images.length} images
+      <ImageDisplay images={images} />
     </div>
   );
 };
