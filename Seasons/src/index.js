2f61bb9c84de0b6b1df33d66526db923532e5a21
Julian Nicholls <juliannicholls29@gmail.com>
2019-11-07 10:04:39 +0000

    Remove constructor and use cDMount in Seasons

--- Seasons/src/index.js
+++ Seasons/src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { coords: null, errorMessage: '' };
 
-    this.state = { coords: null, errorMessage: '' };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ coords: position.coords, errorMessage: '' }),
-      error => {
-        console.error({ error });
-        this.setState({ coords: null, errorMessage: error.message });
-      }
+      position => this.setState({ coords: position.coords }),
+      error => this.setState({ errorMessage: error.message })
     );
   }
 
@@ -20,14 +15,16 @@ class App extends React.Component {
     const { coords, errorMessage } = this.state;
 
     return (
-      <div>
-        Seasons
+      <div className="ui container">
+        <h1>Seasons</h1>
         {!(coords || errorMessage) && <div>Loading...</div>}
         {coords && (
-          <div>
-            <div>Lat: {coords.latitude}</div>
-            <div>Long: {coords.longitude}</div>
-            <div>+- {coords.accuracy}m</div>
+          <div className="ui card">
+            <div className="content">
+              <div>Lat: {coords.latitude}</div>
+              <div>Long: {coords.longitude}</div>
+              <div>+- {coords.accuracy}m</div>
+            </div>
           </div>
         )}
         {errorMessage && <div>Error: {errorMessage}</div>}
