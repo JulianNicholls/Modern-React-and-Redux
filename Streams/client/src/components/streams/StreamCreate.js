88495ec3e08aef5e2aafecad5c0ec698dd954459
Julian Nicholls <juliannicholls29@gmail.com>
2019-11-18 10:40:12 +0000

    Add basic validation to Create form

--- Streams/client/src/components/streams/StreamCreate.js
+++ Streams/client/src/components/streams/StreamCreate.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 const StreamCreate = ({ handleSubmit }) => {
-  const renderInput = ({ input, label }) => {
+  const renderInput = ({ input, label, meta: { touched, error } }) => {
     return (
       <div className="field">
         <label htmlFor={input.name}>{label}</label>
         <input {...input} />
+        {touched && <div>{error}</div>}
       </div>
     );
   };
@@ -25,6 +26,21 @@ const StreamCreate = ({ handleSubmit }) => {
   );
 };
 
+const validate = ({ title, description }) => {
+  const errors = {};
+
+  if (!title || title.length < 15) {
+    errors.title = 'You must enter a title of at least 15 characters';
+  }
+
+  if (!description || description.length < 30) {
+    errors.description = 'You must enter a description of at least 30 characters';
+  }
+
+  return errors;
+};
+
 export default reduxForm({
   form: 'StreamCreate',
+  validate,
 })(StreamCreate);
