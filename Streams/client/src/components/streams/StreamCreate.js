fdcaba38fe91b8b058e1e2a7ef2dd2a6e7d6b689
Julian Nicholls <juliannicholls29@gmail.com>
2019-11-18 10:26:35 +0000

    Wire up redux-form and make basic Create form

--- Streams/client/src/components/streams/StreamCreate.js
+++ Streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>Stream Create</div>;
+const StreamCreate = ({ handleSubmit }) => {
+  const renderInput = ({ input, label }) => {
+    return (
+      <div className="field">
+        <label htmlFor={input.name}>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  };
+
+  const onSubmit = formValues => {
+    console.log({ formValues });
+  };
+
+  return (
+    <form className="ui form" onSubmit={handleSubmit(onSubmit)}>
+      <Field name="title" component={renderInput} label="Title" />
+      <Field name="description" component={renderInput} label="Description" />
+
+      <button className="ui button primary">Create Stream</button>
+    </form>
+  );
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'StreamCreate',
+})(StreamCreate);
