cc6da2419fd8fa071b842478ecad1120c0c785ea
Julian Nicholls <juliannicholls29@gmail.com>
2019-11-18 15:33:41 +0000

    Add minimal stream list

--- Streams/client/src/components/streams/StreamList.js
+++ Streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 
-const StreamList = () => {
-  return <div>Stream List</div>;
+import * as actions from '../../actions';
+
+const StreamList = ({ streams, loadStreams }) => {
+  useEffect(() => loadStreams(), [loadStreams]);
+
+  const renderList = () => {
+    return streams.map(stream => (
+      <div className="item" key={stream.id}>
+        <i className="large middle aligned icon camera" />
+        <div className="content">
+          {stream.title}
+          <div className="description">{stream.description}</div>
+        </div>
+      </div>
+    ));
+  };
+
+  return (
+    <div>
+      <h2>Streams</h2>
+      <div className="ui celled list">{renderList()}</div>
+    </div>
+  );
 };
 
-export default StreamList;
+const mapStateToProps = ({ streams }) => ({ streams: Object.values(streams) });
+
+export default connect(mapStateToProps, actions)(StreamList);
