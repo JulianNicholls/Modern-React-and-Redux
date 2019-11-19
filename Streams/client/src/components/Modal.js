7db383c44a7a2995955258f681c34442d355a0c9
Julian Nicholls <juliannicholls29@gmail.com>
2019-11-19 14:57:06 +0000

    Add props to modal

--- Streams/client/src/components/Modal.js
+++ Streams/client/src/components/Modal.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = () => {
+const Modal = ({ header, content, onDismiss, actions }) => {
   return createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">Modal Content</div>
+    <div className="ui dimmer modals visible active" onClick={onDismiss}>
+      <div
+        className="ui standard modal visible active"
+        onClick={event => event.stopPropagation()}
+      >
+        <div className="header">{header}</div>
+        <div className="content">{content}</div>
+        <div className="actions">{actions}</div>
+      </div>
     </div>,
     document.getElementById('modal')
   );
