e2bf3f87303ab091b9d78837b6b3bc5516c49fa5
Julian Nicholls <juliannicholls29@gmail.com>
2019-11-20 16:27:26 +0000

    Add the video player to StreamShow.
    NB: It didn't really work for me, albeit I was streaming from ffmpeg
    rather than OBS.

--- Streams/client/src/components/streams/StreamShow.js
+++ Streams/client/src/components/streams/StreamShow.js
@@ -1,21 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
+import flv from 'flv.js';
 
 import * as actions from '../../actions';
 
 const StreamShow = ({ match, stream, getStream }) => {
+  const videoRef = useRef();
+  const player = useRef(null);
+
+  const buildPlayer = () => {
+    if (player.current || !stream) return;
+
+    player.current = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${stream.id}.flv`,
+    });
+
+    player.current.attachMediaElement(videoRef.current);
+    player.current.load();
+    console.log('player built');
+  };
+
   useEffect(() => {
-    getStream(match.params.id);
-  }, [getStream, match.params.id]);
+    const { id } = match.params;
 
-  if (!stream) return <div>Loading...</div>;
+    if (!stream) getStream(id);
 
-  const { title, description } = stream;
+    buildPlayer();
+
+    return () => {
+      console.log('Cleaning up');
+      if (player.current) {
+        player.current.destroy();
+        player.current = null;
+      }
+    };
+  });
 
   return (
     <div>
-      <h1>{title}</h1>
-      <h5>{description}</h5>
+      <video ref={videoRef} style={{ width: '100%' }} controls />
+      {stream ? (
+        <>
+          <h1>{stream.title}</h1>
+          <h5>{stream.description}</h5>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 };
