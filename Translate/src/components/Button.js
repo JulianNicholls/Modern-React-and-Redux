635395f7e7030c32fb02719dfaf1c9769ede30b8
Julian Nicholls <juliannicholls29@gmail.com>
2019-11-21 11:52:13 +0000

    Create language context

--- Translate/src/components/Button.js
+++ Translate/src/components/Button.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const Button = () => {
-  return <button className="ui button primary">Submit</button>;
+import LanguageContext from '../context/language';
+
+const submits = {
+  english: 'Submit',
+  french: 'Envoyer',
+  german: 'Senden',
 };
+
+class Button extends React.Component {
+  static contextType = LanguageContext;
+
+  render() {
+    return <button className="ui button primary">{submits[this.context]}</button>;
+  }
+}
 
 export default Button;
--- Translate/src/components/Field.js
+++ Translate/src/components/Field.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-const Field = () => {
-  return (
-    <div className="ui field">
-      <label htmlFor="name">Name</label>
-      <input id="name" type="text" />
-    </div>
-  );
+import LanguageContext from '../context/language';
+
+const names = {
+  english: 'Name',
+  french: 'Nom',
+  german: 'Namen',
 };
+
+class Field extends React.Component {
+  static contextType = LanguageContext;
+
+  render() {
+    return (
+      <div className="ui field">
+        <label htmlFor="name">{names[this.context]}</label>
+        <input id="name" type="text" />
+      </div>
+    );
+  }
+}
 
 export default Field;
